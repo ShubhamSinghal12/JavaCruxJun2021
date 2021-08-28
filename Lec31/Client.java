d747e29366f5879a66a7c8f7348cd8c6df9e6064
Shubham Singhal <shubhamsinghal12@gmail.com>
2021-08-28 22:01:11 +0530

    Lecture 32

--- Lec31/Client.java
+++ Lec31/Client.java
@@ -8,26 +8,37 @@ public class Client {
 		// TODO Auto-generated method stub
 		
 		MyLinkedList ll = new MyLinkedList();
-		for(int i = 5; i > 0; i--)
-		{
-			ll.addFirst(i);
-			ll.display();
-		}
-		ll.addAt(0, 2);
-		ll.display();
+//		for(int i = 5; i > 0; i--)
+//		{
+//			ll.addFirst(i);
+//			ll.display();
+//		}
+//		ll.addAt(0, 2);
+//		ll.display();
+//		
+//		System.out.println(ll.getLast());
+//		
+//		ll.removeAt(5);
+//		ll.display();
+//		ll.removeLast();
+//		ll.display();
+//		
+//		ll.reverseOR();
+//		ll.display();
+//		
+//		LinkedList<Integer> ll1 = new LinkedList<Integer>();
 		
-		System.out.println(ll.getLast());
-		
-		ll.removeAt(5);
-		ll.display();
-		ll.removeLast();
-		ll.display();
-		
-		ll.reverseOR();
-		ll.display();
-		
-		LinkedList<Integer> ll1 = new LinkedList<Integer>();
+//		for(int i = 8; i >= 1; i--)
+//		{
+//			ll.addFirst(i);
+//		}
+//		ll.display();
+//		ll.reverse_k(3);
+//		ll.display();
+//		System.out.println(ll.mid());
 
+		ll.dummyListForCycle();
+		ll.display();
 	}
 
 }
--- Lec31/MyLinkedList.java
+++ Lec31/MyLinkedList.java
@@ -236,5 +236,167 @@ public class MyLinkedList {
 		
 	}
 	
+	public void reverse_k(int k)
+	{
+		head = reverse_k(head,k);
+	}
+	private Node reverse_k(Node n,int k)
+	{
+		if(n == null)
+			return n;
+		
+		Node temp = n;
+		int t = k;
+		while(t>0 && temp != null)
+		{
+			temp = temp.next;
+			t--;
+		}
+		Node prev = reverse_k(temp,k);
+		Node cur = n;
+		
+		while(cur != temp)
+		{
+			Node ahead = cur.next;
+			cur.next = prev;
+			
+			prev = cur;
+			cur = ahead;
+		}
+		
+		return prev;
+		
+	}
+	
+	public int mid()
+	{
+		Node slow = head;
+		Node fast = head;
+		while(fast != null && fast.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		return slow.data;
+	}
+	
+	
+	public void dummyListForIntersection()
+	{
+		Node head1 = null;
+		Node head2 = null;
+		
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		Node n6 = new Node(6);
+		Node n7 = new Node(7);
+		Node n8 = new Node(8);
+		Node n9 = new Node(9);
+		Node n10 = new Node(10);
+		Node n11 = new Node(11);
+		Node n12 = new Node(12);
+		Node n13 = new Node(13);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = n8;
+		n8.next = n9;
+		n9.next = n10;
+		n10.next = null;
+		n11.next = n12;
+		n12.next = n13;
+		n13.next = n7;
+		
+		head1 = n1;
+		head2 = n11;
+		intersection(head1,head2);
+		
+	}
+	
+	private void intersection(Node head1,Node head2)
+	{
+		Node s1 = head1;
+		Node s2 = head2;
+		
+		while(s1 != s2)
+		{
+			if(s1 == null)
+			{
+				s1 = head2;
+			}
+			if(s2 == null)
+			{
+				s2 = head1;
+			}
+			
+			s1 = s1.next;
+			s2 = s2.next;
+		}
+		System.out.println(s1.data);
+	}
+	
+	public void dummyListForCycle()
+	{
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		Node n6 = new Node(6);
+		Node n7 = new Node(7);
+		Node n8 = new Node(8);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = n8;
+		n8.next = n3;
+		
+		head = n1;
+		cycleDR(head);
+		
+	}
+	
+	private void cycleDR(Node head) {
+		
+		Node slow = head;
+		Node fast = head;
+		
+		while(fast != null && fast.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			if(slow == fast)
+			{
+				System.out.println("There is a cycle");
+				break;
+			}
+		}
+		
+		
+		Node temp = head;
+		while(slow.next != temp.next)
+		{
+			slow = slow.next;
+			temp = temp.next;
+		}
+		
+		slow.next = null;
+		
+		
+		
+	}
 	
 }
