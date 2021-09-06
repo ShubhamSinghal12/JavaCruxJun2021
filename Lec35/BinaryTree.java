cd17ba10ef612ad382309e527054411d5933119c
Shubham Singhal <shubhamsinghal12@gmail.com>
2021-09-06 22:03:31 +0530

    Lecture 37

--- Lec35/BinaryTree.java
+++ Lec35/BinaryTree.java
@@ -1,6 +1,10 @@
 package Lec35;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+
 
 
 
@@ -123,8 +127,326 @@ public class BinaryTree {
 			
 		}
 		
+	}
+	
+	public int max()
+	{
+		return max(this.root);
+	}
+	
+	private int max(Node n)
+	{
+		if(n == null)
+		{
+			return Integer.MIN_VALUE;
+		}
+		else
+		{
+			return Math.max(n.data, Math.max(max(n.left), max(n.right)));
+		}
+	}
+	
+	public void preorder()
+	{
+		preorder(this.root);
+	}
+	
+	private void preorder(Node n)
+	{
+		if(n == null)
+			return;
+		
+		System.out.print(n.data+" ");
+		preorder(n.left);
+		preorder(n.right);
 		
 	}
+	
+	public void postorder()
+	{
+		postorder(this.root);
+	}
+	
+	private void postorder(Node n)
+	{
+		if(n == null)
+			return;
+		
+		postorder(n.left);
+		postorder(n.right);
+		System.out.print(n.data+" ");
+		
+	}
+	
+	public void inorder()
+	{
+		inorder(this.root);
+	}
+	
+	private void inorder(Node n)
+	{
+		if(n == null)
+			return;
+		
+		inorder(n.left);
+		System.out.print(n.data+" ");
+		inorder(n.right);
+	}
+	
+	public void levelOrder()
+	{
+		Queue<Node> qt = new LinkedList<>();
+		qt.add(root);
+		while(!qt.isEmpty())
+		{
+			Node n = qt.remove();
+			
+			if(n.left != null)
+				qt.add(n.left);
+			if(n.right != null)
+				qt.add(n.right);
+			
+			System.out.print(n.data+" ");
+		}
+	}
+	
+	public void levelbylevelOrder()
+	{
+		Queue<Node> qt = new LinkedList<>();
+		Queue<Node> temp = new LinkedList<>();
+		qt.add(root);
+		while(!qt.isEmpty())
+		{
+			Node n = qt.remove();
+			
+			if(n.left != null)
+				temp.add(n.left);
+			if(n.right != null)
+				temp.add(n.right);
+			
+			System.out.print(n.data+" ");
+			if(qt.isEmpty())
+			{
+				System.out.println();
+				qt = temp;
+				temp = new LinkedList<>();
+			}
+		}
+	}
+	
+	public void zigzagOrder()
+	{
+		LinkedList<Node> qt = new LinkedList<>();
+		LinkedList<Node> temp = new LinkedList<>();
+		qt.add(root);
+		int lvl = 0;
+		while(!qt.isEmpty())
+		{
+			Node n = qt.removeFirst();
+			
+			if(lvl %2 == 0)
+			{	
+				if(n.left != null)
+					temp.addFirst(n.left);
+				if(n.right != null)
+					temp.addFirst(n.right);
+			}
+			else
+			{
+				if(n.right != null)
+					temp.addFirst(n.right);
+				if(n.left != null)
+					temp.addFirst(n.left);
+			}
+			System.out.print(n.data+" ");
+			if(qt.isEmpty())
+			{
+				lvl++;
+				System.out.println();
+				qt = temp;
+				temp = new LinkedList<>();
+			}
+		}
+	}
+	
+	public int dia()
+	{
+		return dia(this.root);
+	}
+	
+	private int dia(Node n)
+	{
+		if(n == null)
+		{
+			return 0;
+		}
+		else
+		{
+			int l = dia(n.left);
+			int r = dia(n.right);
+			int d = ht(n.left)+ht(n.right)+3;
+			
+			return Math.max(l, Math.max(r, d));
+		}
+	}
+	
+	private class diaPair{
+		int ht = -1;
+		int dia = 0;
+	}
+	
+	public int dia2()
+	{
+		return dia2(this.root).dia;
+	}
+	
+	private diaPair dia2(Node n) {
+		if(n == null)
+		{
+			return new diaPair();
+		}
+		else
+		{
+			diaPair l = dia2(n.left);
+			diaPair r = dia2(n.right);
+			
+			diaPair d = new diaPair();
+			
+			int sd = l.ht + r.ht + 2;
+			
+			d.dia = Math.max(sd, Math.max(l.dia, r.dia));
+			
+			d.ht = Math.max(l.ht, r.ht)+1;
+			
+			return d;
+			
+			
+		}
+		
+	}
+	
+	
+	public boolean isBalanced()
+	{
+		return isBalanced(this.root);
+	}
+	
+	private boolean isBalanced(Node n)
+	{
+		if(n == null)
+		{
+			return true;
+		}
+		else
+		{
+			boolean isb = true;
+			if(Math.abs(ht(n.left)-ht(n.right)) > 1)
+			{
+				isb = false;
+			}
+			
+			return isb && isBalanced(n.left) && isBalanced(n.right);
+		}
+	}
+	
+	
+	private class isBalPair{
+		int ht = -1;
+		boolean isb = true;
+	}
+	
+	
+	public boolean isBalanced2()
+	{
+		return isBalanced2(this.root).isb;
+	}
+	
+	private isBalPair isBalanced2(Node n)
+	{
+		if(n == null)
+		{
+			return new isBalPair();
+		}
+		else
+		{
+			isBalPair tn = new isBalPair();
+			isBalPair l = isBalanced2(n.left);
+			isBalPair r = isBalanced2(n.right);
+			
+			if(Math.abs(l.ht-r.ht) > 1)
+				tn.isb = false;
+			
+			tn.isb = tn.isb && l.isb && r.isb;
+			
+			tn.ht = Math.max(l.ht, r.ht)+1;
+			
+			return tn;
+		}
+		
+	}
+	
+	
+	public void createTreeUsingPreandIn(int[] pre,int[] in)
+	{
+		this.root = createTree(pre, in, 0, pre.length-1, 0, in.length-1);
+	}
+	
+	private Node createTree(int[] pre,int[] in,int plo,int phi,int inlo,int inhi)
+	{
+		if(inlo > inhi)
+		{
+			return null;
+		}
+		else
+		{
+			Node nn = new Node(pre[plo]);
+			int idx = searchIn(in,inlo,inhi,pre[plo]);
+			nn.left = createTree(pre,in, plo+1, plo+idx-inlo, inlo, idx-1);
+			nn.right = createTree(pre,in, plo+idx-inlo+1, phi, idx+1, inhi);
+			return nn;
+		}
+		
+	}
+	
+	private int searchIn(int[] arr, int si, int ei, int item)
+	{
+		for(int i = si; i <= ei; i++)
+		{
+			if(arr[i] == item)
+				return i;
+		}
+		return -1;
+	}
+	
+	
+	
+	static int max_lvl_visited;
+	
+	public void leftTreeView()
+	{
+		max_lvl_visited = -1;
+		leftTreeView(root, 0);
+		System.out.println();
+	}
+	
+	private void leftTreeView(Node n,int lvl)
+	{
+		if(n == null)
+			return;
+		
+		if(lvl > max_lvl_visited)
+		{
+			max_lvl_visited = lvl;
+			System.out.print(n.data+" ");
+		}
+		
+		leftTreeView(n.left, lvl+1);
+		leftTreeView(n.right, lvl+1);
+	}
+	
+	
+	
+	
 	
 	
 	
--- Lec35/Client.java
+++ Lec35/Client.java
@@ -5,11 +5,22 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//10 true 20 true 40 false false true 50 false false true 30 false true 60 false false
+//		BinaryTree bt = new BinaryTree();
+//		bt.createTree();
+//		bt.display();
+//		System.out.println(bt.size());
+//		System.out.println(bt.ht());
+//		System.out.println(bt.max());
+//		System.out.println(bt.dia2());
+		
+		int[] in = {50,20,70,60,100,40,90,80};
+		int[] pre = {100,20,50,60,70,40,80,90};
+		
 		BinaryTree bt = new BinaryTree();
-		bt.createTree();
+		bt.createTreeUsingPreandIn(pre, in);
 		bt.display();
-		System.out.println(bt.size());
-		System.out.println(bt.ht());
+		bt.leftTreeView();
+		System.out.println(bt.isBalanced2());
 
 	}
 
