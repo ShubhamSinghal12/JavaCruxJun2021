36fc034ad72bc25a4d0b94e532ad87c71b1d60c6
Shubham Singhal <shubhamsinghal12@gmail.com>
2021-09-25 21:30:40 +0530

    Lecture 46

--- Lec44/Client.java
+++ Lec44/Client.java
@@ -6,7 +6,7 @@ public class Client {
 		// TODO Auto-generated method stub
 		Graph gp = new Graph(7);
 		gp.addEdge(1, 2, 10);
-		gp.addEdge(1, 4, 60);
+		gp.addEdge(1, 4, 70);
 		gp.addEdge(2,3, 20);
 		gp.addEdge(3, 4, 30);
 		gp.addEdge(4,5, 80);
@@ -15,27 +15,33 @@ public class Client {
 		gp.addEdge(5, 7, 14);
 		
 		gp.display();
-		gp.removeEdge(4, 5);
-//		gp.removeVertex(4);
-		System.out.println("-------");
-		gp.display();
-		System.out.println(gp.noOfEdges());
-//		System.out.println(gp.containsEdge(4, 5));
-		System.out.println(gp.hasPathDFS(1, 6));
+		System.out.println("------------");
+//		gp.removeEdge(4, 5);
+////		gp.removeVertex(4);
+//		System.out.println("-------");
+//		gp.display();
+//		System.out.println(gp.noOfEdges());
+////		System.out.println(gp.containsEdge(4, 5));
+//		System.out.println(gp.hasPathDFS(1, 6));
+//		
+//		gp.DFST();
+//		System.out.println();
+//		System.out.println("--------");
+//		
+////		gp.removeEdge(2, 3);
+////		gp.removeEdge(5, 6);
+//		
+//		System.out.println(gp.isCycle());
+//		System.out.println(gp.noOfConnectedComponenets());
+//		System.out.println(gp.isConnected());
+////		System.out.println(gp.isTree());
+//		System.out.println(gp.connectedComponents());
+////		gp.printPath(1, 6);
 		
-		gp.DFST();
-		System.out.println();
-		System.out.println("--------");
 		
-//		gp.removeEdge(2, 3);
-//		gp.removeEdge(5, 6);
-		
-		System.out.println(gp.isCycle());
-		System.out.println(gp.noOfConnectedComponenets());
-		System.out.println(gp.isConnected());
-//		System.out.println(gp.isTree());
-		System.out.println(gp.connectedComponents());
-//		gp.printPath(1, 6);
+//		gp.Kruskals();
+//		gp.Prims();
+		gp.Dijkstra();
 		
 
 	}
--- Lec44/Graph.java
+++ Lec44/Graph.java
@@ -1,9 +1,12 @@
 package Lec44;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -405,6 +408,256 @@ public class Graph {
 		return ans;
 		
 	}
+	
+	
+	private class DisjointSets{
+		
+		private class Node{
+			int data;
+			int rank;
+			Node parent;
+			
+			public Node(int data,int rank)
+			{
+				this.data = data;
+				this.rank = rank;
+			}
+		}
+		
+		private HashMap<Integer, Node> ds = new HashMap<>();
+		
+		public void createSet(int value)
+		{
+			Node nn = new Node(value,0);
+			nn.parent = nn;
+			ds.put(value, nn);
+		}
+		
+		public int find(int value)
+		{
+			Node f = ds.get(value);
+			return find(f).data;
+		}
+		
+		private Node find(Node n)
+		{
+			if(n.parent == n)
+				return n;
+			else
+				return find(n.parent);
+		}
+		
+		public void union(int u,int v)
+		{
+			Node r1 = ds.get(u);
+			Node r2 = ds.get(v);
+			
+			Node re1 = find(r1);
+			Node re2 = find(r2);
+			
+			if(re1!=re2)
+			{
+//				System.out.println("InUnion: "+ re1.data+" "+re1.rank +" "+re1.parent.data+" "+re2.data+" "+re2.rank+ " "+ re2.parent.data);
+				if(re1.rank > re2.rank)
+				{
+					re2.parent = re1;
+				}
+				else if ( re1.rank < re2.rank)
+				{
+					re1.parent = re2;
+				}
+				else
+				{
+					re2.parent = re1;
+					re1.rank++;
+				}
+			}
+			
+		}
+		
+	}
+	
+	
+	private class EdgePair{
+		int u;
+		int v;
+		int cost;
+		
+		public EdgePair(int u,int v,int cost)
+		{
+			this.u = u;
+			this.v = v;
+			this.cost = cost;
+		}
+		
+		public String toString()
+		{
+			return this.u +" --> "+this.v+" @ "+this.cost;
+		}
+	}
+	
+	public ArrayList<EdgePair> allEdges()
+	{
+		ArrayList<EdgePair> ll = new ArrayList<>();
+		for(int u:graph.keySet())
+		{
+			for(int v: graph.get(u).keySet())
+			{
+				EdgePair edge = new EdgePair(u, v, graph.get(u).get(v));
+				ll.add(edge);
+			}
+		}
+		return ll;
+	}
+	
+	public void Kruskals()
+	{
+		DisjointSets djs = new DisjointSets();
+		for(int v:graph.keySet())
+		{
+			djs.createSet(v);
+		}
+		
+		ArrayList<EdgePair> al = allEdges();
+		
+		Collections.sort(al,new Comparator<EdgePair>() {
+
+			@Override
+			public int compare(EdgePair o1, EdgePair o2) {
+				// TODO Auto-generated method stub
+				return o1.cost-o2.cost;
+			}
+		});
+		
+		for(EdgePair edge:al)
+		{
+			int r1 = djs.find(edge.u);
+			int r2 = djs.find(edge.v);
+			if(r1 != r2)
+			{
+				System.out.println(edge);
+				djs.union(edge.u, edge.v);
+				
+			}
+		}
+	}
+	
+	
+	private class PrimsPair{
+		int vtx;
+		int via;
+		int cost;
+		
+		public PrimsPair(int vtx,int via,int cost)
+		{
+			this.via = via;
+			this.vtx = vtx;
+			this.cost = cost;
+		}
+		
+		public String toString()
+		{
+			return this.vtx + " via "+ this.via + " @ " + this.cost;
+		}
+		
+	}
+	
+	public void Prims()
+	{
+		HashSet<Integer> visited = new HashSet<>();
+		PriorityQueue<PrimsPair> qt = new PriorityQueue<>(new Comparator<PrimsPair>() {
+
+			@Override
+			public int compare(PrimsPair o1, PrimsPair o2) {
+				// TODO Auto-generated method stub
+				return o1.cost-o2.cost;
+			}
+		});
+		
+		qt.add(new PrimsPair(1, 0, 0));
+		
+		while(!qt.isEmpty())
+		{
+			PrimsPair r = qt.remove();
+			
+			if(visited.contains(r.vtx))
+			{
+				continue;
+			}
+			
+			visited.add(r.vtx);
+			if(r.cost != 0)
+				System.out.println(r);
+			for(int nbbr:graph.get(r.vtx).keySet())
+			{
+				if(!visited.contains(nbbr))
+				{
+					qt.add(new PrimsPair(nbbr, r.vtx, graph.get(r.vtx).get(nbbr)));
+				}
+			}
+		}
+		
+	}
+	
+	
+	private class DijkstraPair{
+		int vtx;
+		String via;
+		int cost;
+		
+		public DijkstraPair(int vtx,String via,int cost)
+		{
+			this.via = via;
+			this.vtx = vtx;
+			this.cost = cost;
+		}
+		
+		public String toString()
+		{
+			return this.vtx + " via "+ this.via + " @ " + this.cost;
+		}
+		
+	}
+	
+	public void Dijkstra()
+	{
+		HashSet<Integer> visited = new HashSet<>();
+		PriorityQueue<DijkstraPair> qt = new PriorityQueue<>(new Comparator<DijkstraPair>() {
+
+			@Override
+			public int compare(DijkstraPair o1, DijkstraPair o2) {
+				// TODO Auto-generated method stub
+				return o1.cost-o2.cost;
+			}
+		});
+		
+		qt.add(new DijkstraPair(1, "", 0));
+		
+		while(!qt.isEmpty())
+		{
+			DijkstraPair r = qt.remove();
+			
+			if(visited.contains(r.vtx))
+			{
+				continue;
+			}
+			
+			visited.add(r.vtx);
+			if(r.cost != 0)
+				System.out.println(r);
+			for(int nbbr:graph.get(r.vtx).keySet())
+			{
+				if(!visited.contains(nbbr))
+				{
+					qt.add(new DijkstraPair(nbbr, r.via+" "+r.vtx, r.cost + graph.get(r.vtx).get(nbbr)));
+				}
+			}
+		}
+		
+	}
+
+
+	
 	
 	
 }
